8806fc7f3a5b3ea8b297a49841468363bef89c32
Szymon Kulec <scooletz@gmail.com>
2017-04-04 14:55:10 +0200

    Dummy test added to satisfy build.

---
+++ src/ServiceControl.Monitoring.Tests/DummyTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Monitoring.Tests
+{
+    using NUnit.Framework;
+
+    public class DummyTest
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
