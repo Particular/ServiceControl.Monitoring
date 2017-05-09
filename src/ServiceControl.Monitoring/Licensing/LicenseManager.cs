ce4fc0a9d24e93cba4532350916a96188499150e
Greg Bielleman <greg.bielleman@particular.net>
2017-05-09 14:44:48 +1000

    Add Particular Licensing

---
+++ src/ServiceControl.Monitoring/Licensing/LicenseManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ServiceControl.Monitoring.Licensing
+{
+    using System.IO;
+    using NServiceBus.Logging;
+    using Particular.Licensing;
+
+    public class LicenseManager
+    {
+        internal License Details { get; set; }
+
+        public void Refresh()
+        {
+            Logger.Debug("Checking License Status");
+            var result = ActiveLicense.Find("ServiceControl",
+                new LicenseSourceHKLMRegKey(),
+                new LicenseSourceFilePath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "License", "License.xml")),
+                new LicenseSourceFilePath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ParticularPlatformLicense.xml")));
+
+            if (result.HasExpired)
+            {
+                foreach (var report in result.Report)
+                {
+                    Logger.Info(report);
+                }
+                Logger.Warn("License has expired");
+            }
+            
+            Details = result.License;
+        }
+
+        static readonly ILog Logger = LogManager.GetLogger(typeof(ActiveLicense));
+    }
+    
+}
