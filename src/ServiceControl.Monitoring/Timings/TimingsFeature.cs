114d458c3b8b60dcd155df2bcbb3a795af0b60a1
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-07-11 08:38:41 +0200

    using RegisterSigleton instead of RegisterComponent with DependencyLifecycle.SingleInstance

--- src/ServiceControl.Monitoring/Timings/TimingsFeature.cs
+++ src/ServiceControl.Monitoring/Timings/TimingsFeature.cs
@@ -1,14 +1,13 @@
 ﻿namespace ServiceControl.Monitoring.Timings
 {
-    using NServiceBus;
     using NServiceBus.Features;
 
     public class TimingsFeature : Feature
     {
         protected override void Setup(FeatureConfigurationContext context)
         {
-            context.Container.ConfigureComponent(typeof(ProcessingTimeStore), DependencyLifecycle.SingleInstance);
-            context.Container.ConfigureComponent(typeof(CriticalTimeStore), DependencyLifecycle.SingleInstance);
+            context.Container.RegisterSingleton(typeof(ProcessingTimeStore));
+            context.Container.RegisterSingleton(typeof(CriticalTimeStore));
         }
     }
 }
