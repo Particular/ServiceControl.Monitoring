878ea608626cafd9e0e76fe4b81033336b59758b
Tomasz Masternak <tomasz.masternak@gmail.com>
2017-07-11 13:55:30 +0200

    dto for monitored endpoints list

--- src/ServiceControl.Monitoring/Http/DiagramApiModule.cs
+++ src/ServiceControl.Monitoring/Http/DiagramApiModule.cs
@@ -4,19 +4,17 @@ namespace ServiceControl.Monitoring.Http
     using System.Collections.Generic;
     using System.Linq;
     using Nancy;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
     using Timings;
 
     /// <summary>
     /// Exposes ServiceControl.Monitoring metrics needed for in endpoint overview page.
     /// </summary>
-    public class DiagramsApiModule : ApiModule
+    public class MonitoredEndpointsModule : ApiModule
     {
         /// <summary>
         /// Initializes the metric API module.
         /// </summary>
-        public DiagramsApiModule(ProcessingTimeStore processingTimeStore, CriticalTimeStore criticalTimeStore)
+        public MonitoredEndpointsModule(ProcessingTimeStore processingTimeStore, CriticalTimeStore criticalTimeStore)
         {
             Get["/monitored-endpoints"] = x =>
             {
@@ -28,33 +26,44 @@ namespace ServiceControl.Monitoring.Http
                 var endpointNames = processingTimes.Select(e => e.EndpointName)
                     .Union(criticalTimes.Select(e => e.EndpointName));
 
-                var endpointsResult = new List<JObject>();
+                var endpointsResult = new List<MonitoredEndpoint>();
 
                 foreach (var endpointName in endpointNames)
                 {
                     var processingTime = processingTimes.FirstOrDefault(pt => pt.EndpointName == endpointName);
                     var criticalTime = criticalTimes.FirstOrDefault(ct => ct.EndpointName == endpointName);
 
-                    endpointsResult.Add(new JObject
+                    endpointsResult.Add(new MonitoredEndpoint
                     {
-                        { "name", endpointName },
-                        { "processingTime", new JObject{ 
-                                { "avg", processingTime?.Average },
-                                { "points", new JArray(processingTime?.Intervals.OrderBy(i => i.IntervalStart).Select(i => i.Average)) }
-                            }
+                        Name = endpointName,
+                        ProcessingTime = new MonitoredEndpointTimings
+                        {
+                            Average = processingTime?.Average,
+                            Points = processingTime?.Intervals.OrderBy(i => i.IntervalStart).Select(i => i.Average).ToArray()
                         },
-                        {"criticalTime", new JObject {
-                                { "avg", criticalTime?.Average },
-                                { "points", new JArray(criticalTime?.Intervals.OrderBy(i => i.IntervalStart).Select(i => i.Average)) }
-                            }
+                        CriticalTime = new MonitoredEndpointTimings
+                        {
+                            Average = criticalTime?.Average,
+                            Points = criticalTime?.Intervals.OrderBy(i => i.IntervalStart).Select(i => i.Average).ToArray()
                         }
                     });
                 }
 
-                var result = new JArray(endpointsResult);
-
-                return Response.AsText(result.ToString(Formatting.None), "application/json");
+                return Negotiate.WithModel(endpointsResult.ToArray());
             };
         }
+    }
+
+    public class MonitoredEndpoint
+    {
+        public string Name { get; set; }
+        public MonitoredEndpointTimings ProcessingTime { get; set; }
+        public MonitoredEndpointTimings CriticalTime { get; set; }
+    }
+
+    public class MonitoredEndpointTimings
+    {
+        public double? Average { get; set; }
+        public double[] Points { get; set; }
     }
 }
