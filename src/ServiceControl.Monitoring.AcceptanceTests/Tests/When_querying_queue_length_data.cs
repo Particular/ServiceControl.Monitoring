822daad38b7e5a355872a90641d3908e23c199ed
Tomasz Masternak <tomasz.masternak@particular.net>
2018-05-03 13:30:47 +0200

    Att for queue length default support

--- src/ServiceControl.Monitoring.AcceptanceTests/Tests/When_querying_queue_length_data.cs
+++ src/ServiceControl.Monitoring.AcceptanceTests/Tests/When_querying_queue_length_data.cs
@@ -5,26 +5,26 @@
     using System.Threading.Tasks;
     using AcceptanceTesting;
     using AcceptanceTesting.Customization;
+    using Features;
     using global::Newtonsoft.Json.Linq;
+    using global::ServiceControl.Monitoring;
     using NServiceBus.AcceptanceTests.EndpointTemplates;
     using NUnit.Framework;
-    using ServiceControl.Monitoring;
+    using ServiceControl;
 
     [Category("Integration")]
     public class When_querying_queue_length_data : ApiIntegrationTest
     {
         static string ReceiverEndpointName => Conventions.EndpointNamingConvention(typeof(Receiver));
-
+        static int ReporterQueueLengthValue = 10;
+        
         [Test]
         public async Task When_sending_single_interval_data_Should_report_average_based_on_this_single_interval()
         {
             JToken queueLength = null;
 
             await Scenario.Define<Context>()
-                .WithEndpoint<MonitoredEndpoint>(c =>
-                {
-                    c.When(s => s.SendLocal(new SampleMessage()));
-                })
+                .WithEndpoint<MonitoredEndpoint>()
                 .WithEndpoint<Receiver>()
                 .Done(c => MetricReported("queueLength", out queueLength, c))
                 .Run();
@@ -35,11 +35,8 @@
 
             CollectionAssert.IsNotEmpty(points);
 
-            //NOTE: We can get queue-length value 10 in more than one interval even for single report being sent
-            //      This is caused by the snaposhotting logic that is storing current estimate for queue length periodically
-            //      If the test run crosses interval boundary value 10 can we reported for more than one interval.
-            Assert.IsTrue(points.All(v => v == 10 || v == 0));
-            Assert.IsTrue(points.Any(v => v == 10));
+            Assert.IsTrue(points.All(v => v == ReporterQueueLengthValue || v == 0));
+            Assert.IsTrue(points.Any(v => v == ReporterQueueLengthValue));
         }
 
         class MonitoredEndpoint : EndpointConfigurationBuilder
@@ -48,10 +45,39 @@
             {
                 EndpointSetup<DefaultServer>(c =>
                 {
-#pragma warning disable 618
                     c.EnableMetrics().SendMetricDataToServiceControl(ReceiverEndpointName, TimeSpan.FromSeconds(5));
-#pragma warning restore 618
+                    c.EnableFeature<QueueLengthReporting>();
                 });
+            }
+
+            public class QueueLengthReporting : Feature
+            {
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    context.RegisterStartupTask(b => new QueueLengthReportingTask(b.Build<IReportNativeQueueLength>()));
+                }
+            }
+
+            public class QueueLengthReportingTask : FeatureStartupTask
+            {
+                IReportNativeQueueLength reporter;
+
+                public QueueLengthReportingTask(IReportNativeQueueLength reporter)
+                {
+                    this.reporter = reporter;
+                }
+
+                protected override Task OnStart(IMessageSession session)
+                {
+                    reporter.ReportQueueLength(reporter.MonitoredQueues.First(), ReporterQueueLengthValue);
+
+                    return TaskEx.Completed;
+                }
+
+                protected override Task OnStop(IMessageSession session)
+                {
+                    return TaskEx.Completed;
+                }
             }
         }
 
