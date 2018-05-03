7f9841991aa0664720349914753e922baddc29a6
Mike Minutillo <michael.minutillo@gmail.com>
2018-05-03 15:32:30 +0800

    SQL Native Queue Length

---
+++ src/ServiceControl.Transports.SQLServer/SqlTable.cs
@@ -0,0 +1,115 @@
+﻿namespace ServiceControl.Transports.SQLServer
+{
+    using System;
+    using System.Linq;
+
+    class SqlTable
+    {
+        SqlTable(string name, string schema, string catalog)
+        {
+            QuotedName = SqlNameHelper.Quote(name);
+            QuotedSchema = SqlNameHelper.Quote(schema);
+            QuotedCatalog = SqlNameHelper.Quote(catalog);
+        }
+
+        public string QuotedName { get; }
+        public string UnquotedName => SqlNameHelper.Unquote(QuotedName);
+
+        public string QuotedSchema { get; }
+        public string UnquotedSchema => SqlNameHelper.Unquote(QuotedSchema);
+
+        public string QuotedCatalog { get; }
+        public string UnquotedCatalog => SqlNameHelper.Unquote(QuotedCatalog);
+
+        public override string ToString()
+        {
+            return QuotedCatalog != null ? $"{QuotedCatalog}.{QuotedSchema}.{QuotedName}" : $"{QuotedSchema}.{QuotedName}";
+        }
+
+        public static bool TryParse(string address, int pluginVersion, out SqlTable sqlTable)
+        {
+            if (pluginVersion == 1)
+            {
+                sqlTable = new SqlTable(address.Split('@')[0], "dbo", null);
+                return true;
+            }
+
+            if (pluginVersion == 2)
+            {
+                var parts = address.Split('@').ToArray();
+
+                if (!parts[0].StartsWith("[") || !parts[0].EndsWith("]"))
+                {
+                    parts[0] = $"[{parts[0]}]";
+                }
+
+                sqlTable = new SqlTable(parts[0], parts[1], parts[2]);
+                return true;
+            }
+
+            sqlTable = null;
+            return false;
+        }
+
+        protected bool Equals(SqlTable other)
+        {
+            return String.Equals(QuotedName, other.QuotedName) && String.Equals(QuotedSchema, other.QuotedSchema) && String.Equals(QuotedCatalog, other.QuotedCatalog);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SqlTable) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (QuotedName != null ? QuotedName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (QuotedSchema != null ? QuotedSchema.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (QuotedCatalog != null ? QuotedCatalog.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        class SqlNameHelper
+        {
+            const string prefix = "[";
+            const string suffix = "]";
+
+            public static string Quote(string name)
+            {
+                if (name == null)
+                {
+                    return null;
+                }
+
+                if (name.StartsWith(prefix) && name.EndsWith(suffix))
+                {
+                    return name;
+                }
+
+                return prefix + name.Replace(suffix, suffix + suffix) + suffix;
+            }
+
+            public static string Unquote(string quotedString)
+            {
+                if (quotedString == null)
+                {
+                    return null;
+                }
+
+                if (!quotedString.StartsWith(prefix) || !quotedString.EndsWith(suffix))
+                {
+                    return quotedString;
+                }
+
+                return quotedString
+                    .Substring(prefix.Length, quotedString.Length - prefix.Length - suffix.Length).Replace(suffix + suffix, suffix);
+            }
+        }
+    }
+}
