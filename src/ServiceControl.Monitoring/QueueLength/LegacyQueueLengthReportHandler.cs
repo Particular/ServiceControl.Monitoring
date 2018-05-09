9b843ea131550a6b8ea91ae0bb143edec38d938b
Tomasz Masternak <tomasz.masternak@particular.net>
2018-05-09 13:27:07 +0200

    Legacy queue length logger fix

--- src/ServiceControl.Monitoring/QueueLength/LegacyQueueLengthReportHandler.cs
+++ src/ServiceControl.Monitoring/QueueLength/LegacyQueueLengthReportHandler.cs
@@ -9,18 +9,34 @@
 
     class LegacyQueueLengthReportHandler : IHandleMessages<MetricReport>
     {
-        ConcurrentDictionary<string, string> loggedInstances = new ConcurrentDictionary<string, string>();
+        LegacyQueueLengthEndpoints legacyEndpoints;
+
+        public LegacyQueueLengthReportHandler(LegacyQueueLengthEndpoints legacyEndpoints)
+        {
+            this.legacyEndpoints = legacyEndpoints;
+        }
 
         public Task Handle(MetricReport message, IMessageHandlerContext context)
         {
             var endpointInstanceId = EndpointInstanceId.From(context.MessageHeaders);
 
-            if (loggedInstances.TryAdd(endpointInstanceId.InstanceId, endpointInstanceId.InstanceId))
+            if (legacyEndpoints.TryAdd(endpointInstanceId.InstanceId))
             {
                 Logger.Warn($"Legacy queue length report received from {endpointInstanceId.InstanceName} instance of {endpointInstanceId.EndpointName}");
             }
 
             return TaskEx.Completed;
+        }
+
+        public class LegacyQueueLengthEndpoints
+        {
+            ConcurrentDictionary<string, string> regsteredInstances = new ConcurrentDictionary<string, string>();
+
+            public bool TryAdd(string id)
+            {
+                return regsteredInstances.TryAdd(id, id);
+            }
+
         }
 
         static readonly ILog Logger = LogManager.GetLogger(typeof(LegacyQueueLengthReportHandler));
