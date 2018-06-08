ef1bd297264079d481e1f9d942190eb467103c81
Szymon Kulec <scooletz@gmail.com>
2018-06-08 17:43:51 +0200

    prime bits of SQS Queue Length provider

---
+++ src/ServiceControl.Transports.AmazonSQS/QueueLengthProvider.cs
@@ -0,0 +1,132 @@
+﻿namespace ServiceControl.Transports.AmazonSQS
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Monitoring;
+    using Monitoring.Infrastructure;
+    using Monitoring.Messaging;
+    using Monitoring.QueueLength;
+    using NServiceBus.Logging;
+    using NServiceBus.Metrics;
+
+    public class QueueLengthProvider : IProvideQueueLength
+    {
+        ConcurrentDictionary<EndpointInputQueue, Iqu> queues = new ConcurrentDictionary<EndpointInputQueue, CloudQueue>();
+        ConcurrentDictionary<CloudQueue, int> sizes = new ConcurrentDictionary<CloudQueue, int>();
+        ConcurrentDictionary<CloudQueue, CloudQueue> problematicQueues = new ConcurrentDictionary<CloudQueue, CloudQueue>();
+
+        string connectionString;
+        QueueLengthStore store;
+
+        CancellationTokenSource stop = new CancellationTokenSource();
+        Task pooler;
+
+        public void Initialize(string connectionString, QueueLengthStore store)
+        {
+            this.connectionString = connectionString;
+            this.store = store;
+        }
+
+        public void Process(EndpointInstanceId endpointInstanceId, EndpointMetadataReport metadataReport)
+        {
+            var endpointInputQueue = new EndpointInputQueue(endpointInstanceId.EndpointName, metadataReport.LocalAddress);
+            var queueName = QueueNameHelper.GetSqsQueueName(metadataReport.LocalAddress,);
+
+            var queueClient = CloudStorageAccount.Parse(connectionString).CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+
+            queues.AddOrUpdate(endpointInputQueue, _ => queue, (_, currentQueue) =>
+            {
+                if (currentQueue.Name.Equals(queue.Name) == false)
+                {
+                    sizes.TryRemove(currentQueue, out var _);
+                }
+
+                return queue;
+            });
+
+            sizes.TryAdd(queue, 0);
+        }
+
+        public void Process(EndpointInstanceId endpointInstanceId, TaggedLongValueOccurrence metricsReport)
+        {
+            //HINT: ASQ  server endpoints do not support endpoint level queue length monitoring
+        }
+
+        public Task Start()
+        {
+            stop = new CancellationTokenSource();
+
+            pooler = Task.Run(async () =>
+            {
+                while (!stop.Token.IsCancellationRequested)
+                {
+                    try
+                    {
+                        await FetchQueueSizes().ConfigureAwait(false);
+
+                        UpdateQueueLengthStore();
+
+                        await Task.Delay(QueryDelayInterval);
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.Error("Error querying sql queue sizes.", e);
+                    }
+                }
+            });
+
+            return TaskEx.Completed;
+        }
+
+        public Task Stop()
+        {
+            stop.Cancel();
+
+            return pooler;
+        }
+
+        void UpdateQueueLengthStore()
+        {
+            var nowTicks = DateTime.UtcNow.Ticks;
+
+            foreach (var tableNamePair in queues)
+            {
+                store.Store(
+                    new[]{ new RawMessage.Entry
+                    {
+                        DateTicks = nowTicks,
+                        Value = sizes.TryGetValue(tableNamePair.Value, out var size) ? size : 0
+                    }},
+                    tableNamePair.Key);
+            }
+        }
+
+        Task FetchQueueSizes() => Task.WhenAll(sizes.Select(kvp => FetchLength(kvp.Key)));
+
+        async Task FetchLength(CloudQueue queue)
+        {
+            try
+            {
+                await queue.FetchAttributesAsync(stop.Token).ConfigureAwait(false);
+                sizes[queue] = queue.ApproximateMessageCount.GetValueOrDefault();
+
+                problematicQueues.TryRemove(queue, out _);
+            }
+            catch (Exception ex)
+            {
+                // simple "log once" approach to do not flood logs
+                if (problematicQueues.TryAdd(queue, queue))
+                {
+                    Logger.Error($"Obtaining Azure Storage Queue count failed for '{queue.Name}'", ex);
+                }
+            }
+        }
+
+        static TimeSpan QueryDelayInterval = TimeSpan.FromMilliseconds(200);
+        static ILog Logger = LogManager.GetLogger<QueueLengthProvider>();
+    }
+}
---
+++ src/ServiceControl.Transports.AmazonSQS/QueueNameHelper.cs
@@ -0,0 +1,127 @@
+﻿namespace ServiceControl.Transports.AmazonSQS
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Amazon.S3;
+    using Amazon.SQS;
+    using NServiceBus.Settings;
+
+    static class QueueNameHelper
+    {
+        // copied from https://github.com/Particular/NServiceBus.AmazonSQS/blob/402230de0989ab333124c124cdfdeec14b642263/src/NServiceBus.AmazonSQS/QueueNameHelper.cs
+        public static string GetSqsQueueName(string queue, TransportConfiguration transportConfiguration)
+        {
+            if (string.IsNullOrWhiteSpace(queue))
+            {
+                throw new ArgumentNullException(nameof(queue));
+            }
+
+            var s = transportConfiguration.QueueNamePrefix + queue;
+
+            if (transportConfiguration.PreTruncateQueueNames && s.Length > 80)
+            {
+                var charsToTake = 80 - transportConfiguration.QueueNamePrefix.Length;
+                s = transportConfiguration.QueueNamePrefix +
+                    new string(s.Reverse().Take(charsToTake).Reverse().ToArray());
+            }
+
+            if (s.Length > 80)
+            {
+                throw new Exception($"Address {queue} with configured prefix {transportConfiguration.QueueNamePrefix} is longer than 80 characters and therefore cannot be used to create an SQS queue. Use a shorter queue name.");
+            }
+
+            var skipCharacters = s.EndsWith(".fifo") ? 5 : 0;
+            var queueNameBuilder = new StringBuilder(s);
+
+            // SQS queue names can only have alphanumeric characters, hyphens and underscores.
+            // Any other characters will be replaced with a hyphen.
+            for (var i = 0; i < queueNameBuilder.Length - skipCharacters; ++i)
+            {
+                var c = queueNameBuilder[i];
+                if (!char.IsLetterOrDigit(c)
+                    && c != '-'
+                    && c != '_')
+                {
+                    queueNameBuilder[i] = '-';
+                }
+            }
+
+            return queueNameBuilder.ToString();
+        }
+    }
+
+    class TransportConfiguration
+    {
+        public TransportConfiguration(ReadOnlySettings settings)
+        {
+            // Accessing the settings bag during runtime means a lot of boxing and unboxing,
+            // all properties of this class are lazy initialized once they are accessed
+            this.settings = settings;
+        }
+
+        public Func<IAmazonSQS> SqsClientFactory
+        {
+            get
+            {
+                if (sqsClientFactory == null)
+                {
+                    sqsClientFactory = settings.GetOrDefault<Func<IAmazonSQS>>(SettingsKeys.SqsClientFactory) ?? (() => new AmazonSQSClient());
+                }
+                return sqsClientFactory;
+            }
+        }
+
+        public Func<IAmazonS3> S3ClientFactory
+        {
+            get
+            {
+                if (s3ClientFactory == null)
+                {
+                    s3ClientFactory = settings.GetOrDefault<Func<IAmazonS3>>(SettingsKeys.S3ClientFactory) ?? (() => new AmazonS3Client());
+                }
+                return s3ClientFactory;
+            }
+        }
+
+        public string QueueNamePrefix
+        {
+            get
+            {
+                if (queueNamePrefix == null)
+                {
+                    queueNamePrefix = settings.GetOrDefault<string>(SettingsKeys.QueueNamePrefix);
+                }
+                return queueNamePrefix;
+            }
+        }
+
+        public bool PreTruncateQueueNames
+        {
+            get
+            {
+                if (!preTruncateQueueNames.HasValue)
+                {
+                    preTruncateQueueNames = settings.GetOrDefault<bool>(SettingsKeys.PreTruncateQueueNames);
+                }
+                return preTruncateQueueNames.Value;
+            }
+        }
+
+
+        ReadOnlySettings settings;
+        string queueNamePrefix;
+        bool? preTruncateQueueNames;
+        Func<IAmazonS3> s3ClientFactory;
+        Func<IAmazonSQS> sqsClientFactory;
+
+        static class SettingsKeys
+        {
+            const string Prefix = "NServiceBus.AmazonSQS.";
+            public const string SqsClientFactory = Prefix + nameof(SqsClientFactory);
+            public const string S3ClientFactory = Prefix + nameof(S3ClientFactory);
+            public const string QueueNamePrefix = Prefix + nameof(QueueNamePrefix);
+            public const string PreTruncateQueueNames = Prefix + nameof(PreTruncateQueueNames);
+        }
+    }
+}
