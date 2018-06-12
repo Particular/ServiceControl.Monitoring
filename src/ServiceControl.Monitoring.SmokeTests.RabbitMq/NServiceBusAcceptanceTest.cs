1670b553e77252294eae6ba744d7401db39606fd
Tomasz Masternak <tomasz.masternak@particular.net>
2018-06-12 08:26:41 +0200

    Fixing inspections

--- src/ServiceControl.Monitoring.SmokeTests.RabbitMq/NServiceBusAcceptanceTest.cs
+++ src/ServiceControl.Monitoring.SmokeTests.RabbitMq/NServiceBusAcceptanceTest.cs
@@ -2,7 +2,6 @@ namespace ServiceControl.Monitoring.SmokeTests.RabbitMQ
 {
     using System.Linq;
     using System.Threading;
-    using NServiceBus;
     using NUnit.Framework;
     using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
 
