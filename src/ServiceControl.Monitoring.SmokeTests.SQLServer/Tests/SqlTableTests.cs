b317db458ba73e9f178bc610897cf608e08334dc
Tomasz Masternak <tomasz.masternak@particular.net>
2018-10-02 12:15:14 +0200

    sql ql provider extracts default schema info from the connection string

--- src/ServiceControl.Monitoring.SmokeTests.SQLServer/Tests/SqlTableTests.cs
+++ src/ServiceControl.Monitoring.SmokeTests.SQLServer/Tests/SqlTableTests.cs
@@ -6,18 +6,18 @@
     public class SqlTableTests
     {
         [Test]
-        public void When_no_schema_dbo_is_used_instead()
+        public void When_no_schema_default_is_used_instead()
         {
-            var sqlTable = SqlTable.Parse("Endpoint");
+            var sqlTable = SqlTable.Parse("Endpoint", "dbo");
 
             Assert.AreEqual("Endpoint", sqlTable.UnquotedName);
             Assert.AreEqual("dbo", sqlTable.UnquotedSchema);
         }
 
         [Test]
-        public void When_no_catlog_specified_the_value_in_sqlTable_is_null()
+        public void When_no_catalog_specified_the_value_in_sqlTable_is_null()
         {
-            var sqlTable = SqlTable.Parse("Endpoint@[some-schema]");
+            var sqlTable = SqlTable.Parse("Endpoint@[some-schema]", "dbo");
 
             Assert.AreEqual("Endpoint", sqlTable.UnquotedName);
             Assert.AreEqual(null, sqlTable.UnquotedCatalog);
@@ -26,13 +26,21 @@
         [TestCase("Endpoint@[s]@[c]", "[Endpoint]", "[s]", "[c]")]
         [TestCase("Endpo]int@[schema--x]@[D234F]", "[Endpo]]int]", "[schema--x]", "[D234F]")]
         [TestCase("[Quoted]@[x]@[z]", "[Quoted]", "[x]", "[z]")]
-        public void Endptoint_name_schema_and_catalog_are_parsed_from_address_string_representation(string address, string endpoint, string schema, string catalog)
+        public void Endpoint_name_schema_and_catalog_are_parsed_from_address_string_representation(string address, string endpoint, string schema, string catalog)
         {
-            var sqlTable = SqlTable.Parse(address);
+            var sqlTable = SqlTable.Parse(address, "dbo");
 
             Assert.AreEqual(endpoint, sqlTable.QuotedName);
             Assert.AreEqual(schema, sqlTable.QuotedSchema);
             Assert.AreEqual(catalog, sqlTable.QuotedCatalog);
+        }
+
+        [Test]
+        public void Default_schema_is_used_if_none_is_specified_in_the_address()
+        {
+            var sqlTable = SqlTable.Parse("Endpoint", "custom-schema");
+
+            Assert.AreEqual(sqlTable.UnquotedSchema, "custom-schema");
         }
     }
 }
