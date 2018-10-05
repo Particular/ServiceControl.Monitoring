a3f1806f4347e650fe1b16f33de47807cdef7b82
Tomasz Masternak <tomasz.masternak@particular.net>
2018-10-05 10:33:59 +0200

    type safe RabbitMQ transport configuration

--- src/ServiceControl.Transports.RabbitMQ/ServiceControlRabbitMQTransport.cs
+++ src/ServiceControl.Transports.RabbitMQ/ServiceControlRabbitMQTransport.cs
@@ -1,6 +1,18 @@
 ﻿namespace ServiceControl.Transports.RabbitMQ
 {
-    public class ServiceControlRabbitMQTransport : NServiceBus.RabbitMQTransport
+    using NServiceBus;
+    using NServiceBus.Settings;
+    using NServiceBus.Transport;
+
+    public class ServiceControlRabbitMQTransport : RabbitMQTransport
     {
+        public override TransportInfrastructure Initialize(SettingsHolder settings, string connectionString)
+        {
+            var extensions = new TransportExtensions<RabbitMQTransport>(settings);
+
+            extensions.UseConventionalRoutingTopology();
+
+            return base.Initialize(settings, connectionString);
+        }
     }
 }
